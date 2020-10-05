8511079aed8d58e09477ee83d58404a46500fe24
Kizero1998 <hoangminhdo98@gmail.com>
2020-10-05 14:31:32 +0700

    Add files via upload

--- Factory Pattern/GUNS/main/main.go
+++ Factory Pattern/GUNS/main/main.go
@@ -3,26 +3,30 @@ package main
 import (
 	"fmt"
 	"reflect"
-	)
+	"sync"
+)
+
 type iGun interface {
 	setName(name string)
 	setPower(power int)
 	getName() string
 	getPower() int
 }
-var instance *iGun
-
 
 type gunData struct {
 	name  string
 	power int
+	//NGD []newGunData
 }
-type ak47 struct {
-	gunData
+type newGunData struct {
+	GD []gunData
 }
-type maverick struct {
-	gunData
-}
+//type ak47 struct {
+//	gunData
+//}
+//type maverick struct {
+//	gunData
+//}
 func (g *gunData) setName(name string) {
 	g.name = name
 }
@@ -35,35 +39,52 @@ func (g *gunData) setPower(power int) {
 func (g *gunData) getPower() int {
 	return g.power
 }
-func newAk47() iGun {
-	return &ak47{
-		gunData: gunData{
-			name:  "AK47 gun",
-			power: 4,
-		},
-	}
+
+func (gunData *newGunData) NewGunData() *newGunData {
+	GD := gunData.NewGunData()
+	return GD
 }
-func newMaverick() iGun {
-	return &maverick{
-		gunData: gunData{
-			name:  "Maverick gun",
-			power: 5,
-		},
-	}
-}
-func getGun(gunType string) (iGun, error) {
-	if instance == nil{
-		if gunType == "ak47" {
-			return newAk47(), nil
-		}
-		if gunType == "maverick" {
-			return newMaverick(), nil
-		}
+//func newAk47() iGun {
+//	return &ak47{
+//		gunData: gunData{
+//			name:  "AK47 gun",
+//			power: 4,
+//		},
+//	}
+//}
+//func newMaverick() iGun {
+//	return &maverick{
+//		gunData: gunData{
+//			name:  "Maverick gun",
+//			power: 5,
+//		},
+//	}
+//}
+
+	func getGun(gunType string) (iGun, error) {
+		var singleinstance *iGun
+		var lock = &sync.Mutex{}
+	if singleinstance == nil{
+		lock.Lock()
+		defer lock.Unlock()
+		var gun string
+		var t *gunData
+		gun = reflect.ValueOf(t).MethodByName("").Call([]reflect.Value{
+			reflect.ValueOf(&newGunData{}),
+			reflect.ValueOf(&gunData{name:  "",	power: 0})})[0].String()
+		fmt.Println(gun)
+		//if gunType == "ak47" {
+		//	return newGunData{}, nil
+		//}
+		//if gunType == "maverick" {
+		//	return newMaverick(), nil
+		//}
 		return nil, fmt.Errorf("Wrong gun type passed!")
-		instance = new(iGun)
+		singleinstance = new(iGun)
 	}
 	return getGun(gunType)
 }
+
 func printDetails(g iGun ){
 	t := reflect.TypeOf(g)
 	k := t.Kind()
@@ -91,9 +112,13 @@ func printDetails(g iGun ){
 	//	"%#v", value)
 }
 func main() {
+	println("what gun do you want?")
+//to do code here
+
 	ak47, _ := getGun("ak47")
 	maverick, _ := getGun("maverick")
 	printDetails(ak47)
 	printDetails(maverick)
+
 }
 
